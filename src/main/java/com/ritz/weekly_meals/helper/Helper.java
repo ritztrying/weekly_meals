f53c77f215809a17885a0b0ed4c7ce38683e8e5a
ritesh <ritztrying@gmail.com>
2026-04-05 22:50:24 +0530

    Adding comment

--- src/main/java/com/ritz/weekly_meals/helper/Helper.java
+++ src/main/java/com/ritz/weekly_meals/helper/Helper.java
@@ -5,6 +5,7 @@ import java.time.temporal.WeekFields;
 
 public class Helper {
 
+    // This methode gets current week no of year
     public static int getCurrentWeek() {
         return LocalDate.now().get(WeekFields.ISO.weekOfWeekBasedYear());
     }
